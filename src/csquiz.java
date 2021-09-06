6993eefdc6c4e996a61844acc1e917b4a8907064
Yash <60179298+yashgautam-04@users.noreply.github.com>
2021-09-06 17:29:20 +0530

    Add files via upload

---
+++ src/csquiz.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class csquiz {
+    int qid;
+    String qname,option1,option2,option3,option4;
+    int canswer,companyid;
+    int uanswer;
+
+    public int getQid() {
+        return qid;
+    }
+
+    public void setQid(int qid) {
+        this.qid = qid;
+    }
+
+    public String getQname() {
+        return qname;
+    }
+
+    public void setQname(String qname) {
+        this.qname = qname;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public int getCanswer() {
+        return canswer;
+    }
+
+    public void setCanswer(int canswer) {
+        this.canswer = canswer;
+    }
+
+    public int getCompanyid() {
+        return companyid;
+    }
+
+    public void setCompanyid(int companyid) {
+        this.companyid = companyid;
+    }
+
+    public int getUanswer() {
+        return uanswer;
+    }
+
+    public void setUanswer(int uanswer) {
+        this.uanswer = uanswer;
+    }
+    
+    
+    
+    
+    
+    
+}
